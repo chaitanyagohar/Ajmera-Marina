f80ad0ec9200a23031f2e6401589c9bfd7a2f327
chaitanyagohar <chaitanyagohar@gmail.com>
2025-10-06 09:40:07 +0530

    gtag

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import StickyButtons from "@/components/StickyButtons";
 import GlobalEnquiryProvider from "@/components/GlobalEnquiryProvider";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,12 +12,32 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+
   return (
     <html lang="en" className="scroll-smooth">
       <body className={inter.className}>
         {children}
         <StickyButtons />
         <GlobalEnquiryProvider />
+
+        {/* Google Analytics */}
+        {GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_ID}');
+              `}
+            </Script>
+          </>
+        )}
       </body>
     </html>
   )
